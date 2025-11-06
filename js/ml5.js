226f02987c56ec314ef9ea671dd851cb76aee07e
João Oliveira <joaopedrooliveira6704@gmail.com>
2025-11-06 19:42:12 +0000

    fix: enhance hand drawing logic with connections and stroke settings

--- js/ml5.js
+++ js/ml5.js
@@ -16,9 +16,45 @@ function setup() {
 
 function draw() {
   image(video, 0, 0, width, height);
-
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
+    let connections = [
+      [0, 1],
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [0, 5],
+      [5, 6],
+      [6, 7],
+      [7, 8],
+      [0, 9],
+      [9, 10],
+      [10, 11],
+      [11, 12],
+      [0, 13],
+      [13, 14],
+      [14, 15],
+      [15, 16],
+      [0, 17],
+      [17, 18],
+      [18, 19],
+      [19, 20],
+      [5, 9],
+      [9, 13],
+      [13, 17],
+    ];
+
+    // Desenhar ligações
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    for (let j = 0; j < connections.length; j++) {
+      let [start, end] = connections[j];
+      let kp1 = hand.keypoints[start];
+      let kp2 = hand.keypoints[end];
+      line(kp1.x, kp1.y, kp2.x, kp2.y);
+    }
+
+    // Desenhar pontos
     for (let j = 0; j < hand.keypoints.length; j++) {
       let keypoint = hand.keypoints[j];
       fill(0, 255, 0);
