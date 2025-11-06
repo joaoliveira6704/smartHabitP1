13b7fe3ecdbae5e9d92e388ca50488289ac27de1
migueljcneto <40240358@esmad.ipp.pt>
2025-11-06 16:43:55 +0000

    feat: asset objects and placement

--- js/game.js
+++ js/game.js
@@ -359,7 +359,13 @@ function render() {
         let img = new Image();
         img.src = device.on ? device.dOn : device.dOff;
 
-        ctx.drawImage(img, device.x - camera.x, device.y - camera.y, 48, 48);
+        ctx.drawImage(
+          img,
+          device.x - camera.x,
+          device.y - camera.y,
+          device.width,
+          device.height
+        );
 
         if (device.audioElement) {
           if (device.on) {
