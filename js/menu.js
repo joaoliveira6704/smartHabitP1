4a970397cda9be9f1b09b87921f7a7f4cc5e5520
João Oliveira <joaopedrooliveira6704@gmail.com>
2025-11-07 13:48:08 +0000

    fix: menu breaking css
    fixed menu breaking css due to animation problem

--- js/menu.js
+++ js/menu.js
@@ -2,30 +2,33 @@ const playBtn = document.getElementById("playBtn");
 const instructionsBtn = document.getElementById("instructionsBtn");
 const objectivesBtn = document.getElementById("objectivesBtn");
 const menu = document.getElementById("menu");
+const instructionsMenu = document.getElementById("instructionsMenu");
 const game = document.getElementById("game");
 const menuBtns = document.getElementById("menu-btns");
 const instructionContainer = document.getElementById("instructions-container");
 const logo = document.getElementById("logo");
 const backBtn = document.getElementById("back-btn");
 
+let gameActive = false;
+
 // Start game
 playBtn.addEventListener("click", function () {
   menu.classList.add("hide");
-
   setTimeout(function () {
     menu.style.display = "none";
     game.classList.add("show");
-    game.style.visibility = "visible"; // show canvas area
+    game.style.visibility = "visible";
+    gameActive = true;
   }, 500);
 });
 
 backBtn.addEventListener("click", function () {
-  menu.classList.add("hide");
+  instructionsMenu.classList.add("hide");
   setTimeout(function () {
-    logo.style.display = "block";
+    instructionsMenu.style.display = "none";
+    instructionsMenu.classList.remove("show");
+    menu.style.display = "block";
     menu.classList.remove("hide");
-    instructionContainer.style.display = "none";
-    menuBtns.style.display = "block";
   }, 500);
 });
 
@@ -33,20 +36,19 @@ backBtn.addEventListener("click", function () {
 instructionsBtn.addEventListener("click", function () {
   menu.classList.add("hide");
   setTimeout(function () {
-    logo.style.display = "none";
-    menu.classList.remove("hide");
-    menuBtns.classList.add("show");
-    instructionContainer.style.display = "block";
-    menuBtns.style.display = "none";
+    menu.style.display = "none";
+    instructionsMenu.style.display = "block";
+    instructionsMenu.classList.add("show");
   }, 500);
 });
 
 // Return to menu with "P"
 document.addEventListener("keydown", function (event) {
-  if (event.key.toLowerCase() === "p") {
-    // Hide game visually but keep it active
+  if (event.key.toLowerCase() === "p" && gameActive) {
+    // Hide game
     game.classList.remove("show");
     game.style.visibility = "hidden";
+    gameActive = false;
 
     // Show menu again
     menu.style.display = "block";
